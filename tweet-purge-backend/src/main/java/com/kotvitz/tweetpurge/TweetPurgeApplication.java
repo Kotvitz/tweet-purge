1b3eed54bd1f2c6eaf92df2f5323de546b9a2afc
Michał Kotfica <kotvitz17@gmail.com>
2024-01-24 01:18:41 +0100

    initial commit

---
+++ tweet-purge-backend/src/main/java/com/kotvitz/tweetpurge/TweetPurgeApplication.java
@@ -0,0 +1,13 @@
+package com.kotvitz.tweetpurge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetPurgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetPurgeApplication.class, args);
+	}
+
+}
---
+++ tweet-purge-backend/src/test/java/com/kotvitz/tweetpurge/TweetPurgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotvitz.tweetpurge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetPurgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
