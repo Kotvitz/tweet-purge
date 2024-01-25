37bcdf86b571dff27f006b84c1e4386dc7e7ae67
Michał Kotfica <kotvitz17@gmail.com>
2024-01-25 18:01:45 +0100

    add ngModel values to form and function to clear filters

--- tweet-purge-frontend/src/app/home/home.component.ts
+++ tweet-purge-frontend/src/app/home/home.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  allWords: string = '';
+  exactPhrase: string = '';
+  anyWords: string = '';
+  noneWords: string = '';
+  dateRangeStart: string = '';
+  dateRangeEnd: string = '';
 
+  clearFilters() {
+    this.allWords = '';
+    this.exactPhrase = '';
+    this.anyWords = '';
+    this.noneWords = '';
+    this.dateRangeStart = '';
+    this.dateRangeEnd = '';
+  }
 }
