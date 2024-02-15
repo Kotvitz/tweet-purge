f9f719bd3f8ac8c749bc315e7df56b2cfd757f35
Michał Kotfica <kotvitz17@gmail.com>
2024-02-16 00:03:51 +0100

    add new routes, especially for login callback

--- tweet-purge-frontend/src/app/app-routing.module.ts
+++ tweet-purge-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { LoginCallbackComponent } from './login-callback/login-callback.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'login-callback', component: LoginCallbackComponent },
+  { path: '**', redirectTo: '/' }
 ];
 
 @NgModule({
